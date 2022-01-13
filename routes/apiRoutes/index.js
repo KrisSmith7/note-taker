0089ea323696c78054fe42ac5362b1ff1620103e
KrisSmith7 <krissmithcodes@gmail.com>
2022-01-13 14:28:03 -0500

    corrected htmlroutes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,28 +1,4 @@
-const router = require('express').Router();
-const path = require ('path');
-const fs = require ('fs');
-const dataFile = require('../../db/db.json');
-// const { getNotes, saveNote, deleteNote } = require ('../../Develop/public/assets/js/index')
-
-// Pull from db.json
-router.get("/notes", function (req, res) {
-  let notes = dataFile;
-  console.log(`${req.method} received. This is Notes.`)
-  res.json(notes)
-})
-
-
-// post to db.json
-router.post("/notes", function (req, res) {
-
-  // fs.writeFile('../../db/db.json',(err, data) => {
-  //   if (err) throw err;
-  
-    let notes = JSON.stringify(req.body)
-    console.log(`${req.method} received. This is Notes.`, notes)
-    res.json(notes)
-  })
-  // });
+const router = require ('express').Router()
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express');
-const fs = require('fs');
-const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./routes/apiRoutes/index');
-const htmlRoutes = require('./routes/htmlRoutes/index');
-const path = require ('path');
+const app = express();
+const htmlRoutes = require ('./routes/htmlRoutes/');
+const apiRoutes = require ('./routes/apiRoutes');
 
-app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-app.use(express.static('./public'));
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
+app.use(express.json());
 
+app.use('/api', apiRoutes);
+
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
